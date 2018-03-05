04a5d4f62789191b1e25af132e405e7d7f131b7d
JashDave <jashdave23@gmail.com>
2018-03-05 16:30:32 +0530

    Update bash_help.sh

--- bash_scripts/bash_help.sh
+++ bash_scripts/bash_help.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#This line gives the absolute path of this file (i.e. itself). Can be used to do relative refrencing from this path
+#This line gives the absolute path of this file's parent directory. Can be used to do relative refrencing from this path
 MYPATH="$(cd "$(dirname "$0")";pwd)"
-echo "Abs path to this file -> $MYPATH"
+echo "Abs path to this file's parent directory -> $MYPATH"
